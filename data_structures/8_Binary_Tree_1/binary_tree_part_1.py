c4ea133af7a0aaab7bf2c2c4b10c6350c50e6d85
Koech debby <99505478+Koech6508@users.noreply.github.com>
2025-01-06 11:28:56 +0300

    Update binary_tree_part_1.py

--- data_structures/8_Binary_Tree_1/binary_tree_part_1.py
+++ data_structures/8_Binary_Tree_1/binary_tree_part_1.py
@@ -48,6 +48,55 @@ class BinarySearchTreeNode:
 
         return elements
 
+#finding  the  minimum element in the entire binary tree
+  def find_min (self, data):
+      if self.left = None:
+         return self.data
+      return  self.left.find_min()
+    
+# finding the maximum element in the entire binary  tree
+  def find_max (self, data):
+      if self.right = None:
+          return self.data
+      return  self.right.find_min()
+
+#calculating sum of all elements
+
+  def calculate_sum(self):
+       total = self.data
+
+     if  self.right:
+         total +=self.right.calculate_sum() 
+
+     if  self.left:
+         total += self.left.calculate_sum()
+
+
+#post_order_traversal
+  def post_order_traversal(self):
+          elements = []
+          if self.left:
+            elements += self.left.post_order_traversal()
+
+          if self.right:
+            elements += self.right.post_order_traversal()
+            
+        elements.append(self.data)
+        return elements
+
+
+  def pre_order_traversal(self):
+          elements = []
+          if self.left:
+            elements += self.left.pre_order_traversal()
+
+          if self.right:
+            elements += self.right.pre_order_traversal()
+            
+       
+        return elements   
+
+
 
 def build_tree(elements):
     print("Building tree with these elements:",elements)
@@ -58,6 +107,9 @@ def build_tree(elements):
 
     return root
 
+
+
+
 if __name__ == '__main__':
     countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
     country_tree = build_tree(countries)
@@ -67,3 +119,12 @@ if __name__ == '__main__':
 
     numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
     print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
+
+
+
+    print("In-order Traversal:", numbers_tree.in_order_traversal())
+    print("Minimum value:", numbers_tree.find_min())
+    print("Maximum value:", numbers_tree.find_max())
+    print("Sum of all elements:", numbers_tree.calculate_sum())
+    print("Post-order Traversal:", numbers_tree.post_order_traversal())
+    print("Pre-order Traversal:", numbers_tree.pre_order_traversal())
